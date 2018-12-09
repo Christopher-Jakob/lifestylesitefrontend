bed3de680155d731056bffca1da82c0deb0f9b31
Christopher Jakob <christopher.jakob@S03068-MBPR.local>
2018-12-09 15:08:25 -0600

    added http service

---
+++ src/app/services/httpservice/httpservice.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class Httpservice{
+
+  constructor(private http: HttpClient){}
+
+  get(url){
+    return this.http.get(url);
+  }
+
+  delete(url){
+    return this.http.delete(url);
+  }
+
+  put(url, payload){
+    return this.http.put(url, payload);
+  }
+
+  post(url, payload){
+    return this.http.put(url, payload);
+  }
+}
